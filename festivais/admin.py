790e801b2fa3926450d1c42c02e808e5145b31cb
Tiago Santos <a22403423@alunos.ulht.pt>
2026-03-24 13:52:38 +0000

    Ficha 5

--- festivais/admin.py
+++ festivais/admin.py
@@ -1,14 +1,19 @@
-# admin.py
 from django.contrib import admin
-from .models import Festival, Banda
+from .models import Genero, Banda, Festival
 
-@admin.register(Festival)
-class FestivalAdmin(admin.ModelAdmin):
-    list_display = ("nome", "local")
+class GeneroAdmin(admin.ModelAdmin):
+    list_display = ("nome",)
     search_fields = ("nome",)
 
-
-@admin.register(Banda)
 class BandaAdmin(admin.ModelAdmin):
-    list_display = ("nome", "estilo")
+    list_display = ("nome", "genero")
     search_fields = ("nome",)
+
+class FestivalAdmin(admin.ModelAdmin):
+    list_display = ("nome", "localizacao")
+    search_fields = ("nome",)
+    filter_horizontal = ("bandas",) 
+
+admin.site.register(Genero, GeneroAdmin)
+admin.site.register(Banda, BandaAdmin)
+admin.site.register(Festival, FestivalAdmin)
--- festivais/models.py
+++ festivais/models.py
@@ -1,17 +1,25 @@
-# models.py
 from django.db import models
 
-class Festival(models.Model):
-    nome = models.CharField(max_length=100)
-    local = models.CharField(max_length=100)
+# Create your models here.
+from django.db import models
+
+class Genero(models.Model):
+    nome = models.CharField(max_length=50)
 
     def __str__(self):
         return self.nome
 
-
 class Banda(models.Model):
     nome = models.CharField(max_length=100)
-    estilo = models.CharField(max_length=50)
+    genero = models.ForeignKey(Genero, on_delete=models.PROTECT, related_name='bandas')
+
+    def __str__(self):
+        return self.nome
+
+class Festival(models.Model):
+    nome = models.CharField(max_length=150)
+    localizacao = models.CharField(max_length=100)
+    bandas = models.ManyToManyField(Banda, related_name='festivais')
 
     def __str__(self):
         return self.nome
